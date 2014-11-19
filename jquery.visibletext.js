d312e8928c3ffeacf37f2caf9d3adb8ff605a415
Steven Lindberg <stevenjohnlindberg@gmail.com>
2014-11-19 10:37:47 -0800

    Wrap in an IIFE, stop referencing '$' directly

--- jquery.visibletext.js
+++ jquery.visibletext.js
@@ -1,10 +1,12 @@
-jQuery.fn.visibleText = function() {
-  return $.map(this.contents(), function(el) {
-    if (el.nodeType === 3) {
-      return $(el).text();
-    }
-    if ($(el).is(':visible')) {
-      return $(el).visibleText();
-    }
-  }).join('');
-};
+;(function($) {
+  $.fn.visibleText = function() {
+    return $.map(this.contents(), function(el) {
+      if (el.nodeType === 3) {
+        return $(el).text();
+      }
+      if ($(el).is(':visible')) {
+        return $(el).visibleText();
+      }
+    }).join('');
+  };
+}(jQuery));
